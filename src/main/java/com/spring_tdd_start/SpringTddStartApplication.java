acebe52f1cb1323f7e490fea5c517fd042258d37
= <=>
2021-11-29 21:44:30 +0900

    feat: init project

---
+++ src/main/java/com/spring_tdd_start/SpringTddStartApplication.java
@@ -0,0 +1,13 @@
+package com.spring_tdd_start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTddStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTddStartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_tdd_start/SpringTddStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_tdd_start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTddStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
