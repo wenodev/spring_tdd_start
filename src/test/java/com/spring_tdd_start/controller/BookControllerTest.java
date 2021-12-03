f66a1ef66b9649414ebcfcd86ab6f6ca8ead9cf9
wenodev <logicalstark1105@gmail.com>
2021-12-03 23:33:07 +0900

    feat: add test get

--- src/test/java/com/spring_tdd_start/controller/BookControllerTest.java
+++ src/test/java/com/spring_tdd_start/controller/BookControllerTest.java
@@ -1,10 +1,21 @@
 package com.spring_tdd_start.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @WebMvcTest
 class BookControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void test_get(){
+
+    }
 
 }
