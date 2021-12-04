28003ee338a9d7625c843155639626b2b8c8a192
손주원 <weno@sonjuwons-MacBook-Air.local>
2021-12-04 16:13:10 +0900

    test: test getBook

--- src/test/java/com/spring_tdd_start/controller/BookControllerTest.java
+++ src/test/java/com/spring_tdd_start/controller/BookControllerTest.java
@@ -5,7 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 class BookControllerTest {
@@ -14,8 +15,9 @@ class BookControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    void test_get(){
-
+    void test_get() throws Exception {
+        mockMvc.perform(get("/books" + "/" + 1L))
+                .andExpect(status().isOk());
     }
 
 }
