fcc63294b78da860628aea92ec21b2f188079d18
wenodev <logicalstark1105@gmail.com>
2021-12-06 18:40:18 +0900

    feat: controller done

--- src/main/java/com/spring_tdd_start/domain/Book.java
+++ src/main/java/com/spring_tdd_start/domain/Book.java
@@ -5,6 +5,18 @@ public class Book {
     private String name;
     private String author;
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
     public Book(Long id, String name, String author) {
         this.id = id;
         this.name = name;
