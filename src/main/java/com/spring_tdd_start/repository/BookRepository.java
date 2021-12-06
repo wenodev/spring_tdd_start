127a9f5406a961aad61dfa40a711648785d3dd72
wenodev <logicalstark1105@gmail.com>
2021-12-07 00:37:02 +0900

    feat: add service layer

---
+++ src/main/java/com/spring_tdd_start/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.spring_tdd_start.repository;
+
+import com.spring_tdd_start.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findById(Long id);
+}
