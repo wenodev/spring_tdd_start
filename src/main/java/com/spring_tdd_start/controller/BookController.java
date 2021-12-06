5ce6feec620b3603da41b8a54f99ed5cfba4ec7b
wenodev <logicalstark1105@gmail.com>
2021-12-06 18:40:18 +0900

    feat: controller done

--- src/main/java/com/spring_tdd_start/controller/BookController.java
+++ src/main/java/com/spring_tdd_start/controller/BookController.java
@@ -1,6 +1,7 @@
 package com.spring_tdd_start.controller;
 
 import com.spring_tdd_start.domain.Book;
+import com.spring_tdd_start.service.BookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BookController {
 
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
     @GetMapping("/books/{id}")
-    public Book getBook(@PathVariable Long id) {
-        return new Book(id, "tdd", "kentbeck");
+    public Book detail(@PathVariable Long id) {
+        return bookService.detail(id);
     }
 
 }
