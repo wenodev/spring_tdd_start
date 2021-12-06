6ef36509b1b4ffb9da50ccacbcfcf9fa8d8e7219
wenodev <logicalstark1105@gmail.com>
2021-12-07 00:37:02 +0900

    feat: add service layer

--- src/test/java/com/spring_tdd_start/controller/BookControllerTest.java
+++ src/test/java/com/spring_tdd_start/controller/BookControllerTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.any;
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
---
+++ src/test/java/com/spring_tdd_start/service/BookServiceTest.java
@@ -0,0 +1,25 @@
+package com.spring_tdd_start.service;
+
+import com.spring_tdd_start.domain.Book;
+import com.spring_tdd_start.repository.BookRepository;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+class BookServiceTest {
+
+    private BookRepository bookRepository = mock(BookRepository.class);
+    private BookService bookService = new BookService(bookRepository);
+
+    @Test
+    void detail() {
+        given(bookRepository.findById(anyLong())).willReturn(Optional.of(new Book(1L, "테스트주도개발", "켄트벡")));
+        Book book = bookService.detail(1L);
+        assertThat(book.getName()).isEqualTo("테스트주도개발");
+    }
+}
